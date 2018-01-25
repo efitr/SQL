5342ef86a1dc90d8d322301a615456d08f3c6bc2
Egon Fiedler <efiedler93@gmail.com>
2018-01-25 01:03:44 -0800

    HackerRank Challenges

---
+++ HackerRank/query.sql
@@ -0,0 +1,6 @@
+
+HackerRank Challenge 1:
+Question : Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+SELECT * FROM CITY
+WHERE POPULATION > 100000
+AND COUNTRYCODE = 'USA'; 
